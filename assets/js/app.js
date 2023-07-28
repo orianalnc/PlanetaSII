9061ebd7abba1e290a3bb7620ad4ec6c4ed44045
Orianalnc <orianalnc94@gmail.com>
2023-07-28 16:32:49 -0400

    videos

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,5 +35,24 @@ const swiper = new Swiper(".welcome_planeta__slider", {
 //
 
 
+function searchFilterCurso(){
+    let selectedCurso = document.getElementById("filterbyCurso").value;
+    let selectedAsignatura = document.getElementById("filterbyAsignatura").value;
+    let selectedOA = document.getElementById("filterbyAo").value;
+    console.log(selectedCurso, selectedAsignatura, selectedOA);
+    const input = document.querySelector(".form-select");
+    const cards = document.getElementsByClassName("fichas_docentes__contenidos_card");
+    console.log(cards[1])
+    //let textBox = input.value;
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".card");
+        if ((cards[i].classList.contains(selectedCurso) || selectedCurso == "") && (cards[i].classList.contains(selectedAsignatura) || selectedAsignatura == "") && (cards[i].classList.contains(selectedOA) || selectedOA == "")  ) {
+            cards[i].classList.remove("d-none");
+        } else {
+            cards[i].classList.add("d-none");
+        }
+    }
+}
+
 
 
